ad9d7e4e7f71defba75e508c246e0104dae52896
Lander Van laer <lander.vanlaer1@gmail.com>
2020-10-07 15:44:40 +0200

    fix: Todo don't refresh after clicking checkbox

--- src/js/articles/properties_menu.js
+++ src/js/articles/properties_menu.js
@@ -3,15 +3,19 @@
  */
 const query = new URLSearchParams(location.search);
 /**
+ * All the checkboxes on the page in the form properties
  * @type HTMLFormElement
  */
 const properties = document.querySelector("body > main > aside > form[name='properties']");
+
+/**
+ * An array of checkboxes
+ * @type {Array<HTMLInputElement>}
+ * @see {@link properties} for further information
+ */
 const ELEMENTS = [...properties.elements];
 
-properties.addEventListener('submit', e => {
-    e.preventDefault();
-    return false;
-});
+properties.addEventListener('submit', e => e.preventDefault());
 
 // check checkboxes when in GET query
 query.forEach((v, k) => {
@@ -22,8 +26,20 @@ query.forEach((v, k) => {
 });
 
 //Submit on check
-ELEMENTS.forEach(e => e.addEventListener('change', () => {
-    properties.submit();
-}));
+ELEMENTS.forEach(e => e.addEventListener('change', submit));
 
-// TODO don't refresh after clicking checkbox
+/**
+ * Changin URL by the {@link properties} without reloading the page
+ */
+function submit() {
+    const params = new URLSearchParams();
+    ELEMENTS.forEach(e => {
+        switch (e.type) {
+            case 'checkbox':
+                if (e.checked)
+                    params.append(e.name, 'on');
+                break;
+        }
+    })
+    history.pushState(null, document.title, location.pathname + "?" + params.toString());
+}
