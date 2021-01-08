8ecb38c53db53b0644bfddf4cff8c2bb2e376db8
Lander Van laer <lander.vanlaer1@gmail.com>
2021-01-08 22:12:17 +0100

    Create TEMPLATE file

---
+++ src/TEMPLATE.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include "resources/head.html" ?>
+</head>
+
+<body>
+    <?php include "resources/header.php" ?>
+    <main>
+
+    </main>
+    <?php include "resources/footer.php" ?>
+</body>
+
+</html>
