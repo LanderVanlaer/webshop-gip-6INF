d0cb0e4ca15946580a86054578b2b7c19da84e8f
Lander Van laer <lander.vanlaer1@gmail.com>
2021-02-23 22:33:38 +0100

    Update connection.inc.php, thr to die

--- src/includes/connection.inc.php
+++ src/includes/connection.inc.php
@@ -1,6 +1,5 @@
 <?php
     //https://www.php.net/manual/en/mysqlinfo.api.choosing.php
     $mysqli = new mysqli("localhost:3306", "root", "usbw", "gigacam");
-    if ($mysqli->connect_errno) {
-        throw new mysqli_sql_exception("Error with connection of database");
-    }
+
+    if ($mysqli->connect_errno) die("Error with connection to database {$mysqli->connect_error}");
