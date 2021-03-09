fe51de5847395a09740ec31a8a414f539a850f4d
Lander Van laer <lander.vanlaer1@gmail.com>
2021-03-09 13:58:08 +0100

    Head admin update

--- src/resources/admin/head.php
+++ src/resources/admin/head.php
@@ -1,4 +1,6 @@
-<?php
-    include __DIR__ . "/../head.html";
-?>
+<link rel="stylesheet" href="/css/style.css">
+<meta name="theme-color" content="#ffffff">
+<link rel="icon" href="/images/logo_basic.svg">
+<link rel="mask-icon" href="/images/logo_basic.svg" color="#000000">
+<link rel="apple-touch-icon" href="/images/logo_basic.svg">
 <link rel="stylesheet" href="/css/admin/style.css">
