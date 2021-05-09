8ba27e1caf2c3f2d977e773617bc9067522108f7
Lander Van laer <lander.vanlaer1@gmail.com>
2021-05-09 13:34:56 +0200

    h_captcha update php admin login

--- src/admin/login/index.php
+++ src/admin/login/index.php
@@ -6,32 +6,19 @@
     include "../../includes/validateFunctions.inc.php";
     include "../../includes/basicFunctions.inc.php";
     include "../../includes/admin/adminFunctions.inc.php";
-
-    const H_CAPTCHA_API_KEY = '3e7a1904-27c7-4bfc-b392-99c68f690f23';
+    include "../../includes/h_captcha.inc.php";
 
     session_start();
 
     if (isAdmin()) redirect("/admin");
 
     $error = 0;
-    $one_empty = $not_found = false;
 
     if (!empty($_POST)) {
-        $data = array(
-                'secret' => "0x1D3C92B9e7D52594976A623BFE8f870884e4A154",
-                'response' => $_POST['h-captcha-response']
-        );
+        $responseData = h_captcha_verify($_POST['h-captcha-response']);
 
-        $verify = curl_init();
-        curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
-        curl_setopt($verify, CURLOPT_POST, true);
-        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($verify);
-
-        $responseData = json_decode($response);
         if (!$responseData->success) {
-            $error = Error::h_captscha;
+            $error = Error::h_captcha;
             goto end;
         }
 
