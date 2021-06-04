f2cf20e7d91c0dc1d40266eed850438f3d44b12c
Lander Van laer <lander.vanlaer1@gmail.com>
2021-06-04 15:08:43 +0200

    Add mission and vision

--- src/about/index.php
+++ src/about/index.php
@@ -12,8 +12,8 @@
             padding-right: 0;
         }
 
-        h1 {
-            padding-left: 1rem;
+        h1, section {
+            padding: 1rem;
         }
 
         object[type="application/pdf"] {
@@ -32,6 +32,21 @@
         <object data="/Ondernemingsplan.pdf" type="application/pdf">
             <p>Click <a href="/Ondernemingsplan.pdf" target="_blank">HERE</a> to get the PDF!</p>
         </object>
+        <section class="mission-vision">
+            <section class="mission">
+                <h3>Mission</h3>
+                <p>
+                    Het helpen en het beste bieden voor de foto’s van de professionele en hobbyfotograaf.
+                </p>
+            </section>
+            <section class="vision">
+                <h3>Vission</h3>
+                <p>
+                    Wij willen zo veel mogelijk kwalitatieve foto geassocieerde producten verkopen, zodat elke fotograaf met zijn/haar budget de benodigde producten vindt
+                    op één gezamenlijke plek.
+                </p>
+            </section>
+        </section>
     </main>
     <?php include "../resources/footer.php" ?>
 </body>
