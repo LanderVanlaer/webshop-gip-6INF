fe1c0e93ba7919a3b59dac34d47044bc94c089a5
Lander Van laer <lander.vanlaer1@gmail.com>
2021-06-06 10:22:27 +0200

    Add language-support Error-class

--- src/includes/Error.php
+++ src/includes/Error.php
@@ -2,7 +2,11 @@
 
     namespace includes;
 
-    abstract class Error {
+    include_once "basicFunctions.inc.php";
+    const __LANGUAGE_JSON_FILE_NAME_ERROR_MESSAGES__ = __DIR__ . "\\..\\resources\\languages\\error.messages.json";
+
+    abstract class Error
+    {
         const empty_value = 1;
         const duplicate = 2;
         const file_not_allowed = 3;
@@ -19,71 +23,35 @@
         const bad_request = 15; //400
         const not_modified = 16; //304
 
+
+        public static final function print_admin_message($error, $executed = false, $succes = false, $last_id = 0) {
+            if (empty($error) && empty($executed)) return;
+
+            if (!empty($error)) {
+                self::print_message(str_replace("{id}", $last_id, self::get_error_messages()[$error]));
+                return;
+            }
+
+            if (!$executed) return;
+
+            if (!$succes) {
+                self::print_message(self::get_error_messages()['executed']);
+                return;
+            };
+
+            if ($last_id) {
+                self::print_message(str_replace("{id}", $last_id, self::get_error_messages()['succes_last_id']));
+                return;
+            }
+
+            self::print_message(self::get_error_messages()['succes']);
+        }
+
         public static final function print_message($str) {
             echo "<div class='message'>$str</div>";
         }
 
-        public static final function print_admin_message($error, $executed = false, $succes = false, $last_id = 0) {
-            if ($error || $executed) {
-                echo "<div class='message'>";
-                switch ($error) {
-                    case Error::empty_value:
-                        echo "Gelieve alle verplichte velden in te vullen";
-                        break;
-                    case Error::file_not_allowed:
-                        echo "U kan geen bestanden uploaden van dit type";
-                        break;
-                    case Error::file_too_large:
-                        echo "Het bestand is te groot";
-                        break;
-                    case Error::duplicate:
-                        echo "Er bestaat er al een met deze naam, id: $last_id";
-                        break;
-                    case Error::password_not_match:
-                        echo "De 2 wachtwoorden zijn niet gelijk aan elkaar";
-                        break;
-                    case Error::h_captcha:
-                        echo "Gelieve de hCaptcha in te vullen";
-                        break;
-                    case Error::login_fail:
-                        echo "Wachtwoord of username fout";
-                        break;
-                    case Error::email_not_valid:
-                        echo "Het gegeven emailadres is niet geldig";
-                        break;
-                    case Error::duplicate_customer:
-                        echo "Er bestaat al een account met deze email";
-                        break;
-                    case Error::password_not_possible:
-                        echo "Wachtwoord moet uit minstens 1 kleine letter, 1 hoofdletter, 1 cijfer en minimum 8 karakters bestaan en maximaal uit 32";
-                        break;
-                    case Error::customer_not_found_by_email:
-                        echo "Er bestaat geen gebruiker met deze email";
-                        break;
-                    case Error::customer_not_active:
-                        echo "Gelieve uw mailadres te verifiëren";
-                        break;
-                    case Error::bad_request:
-                        echo "Er is iets fout gelopen bij de aanvraag, gelieve later terug opnieuw te proberen";
-                        break;
-                    case Error::internal:
-                        echo "Internal Error, please contact the customer service or try again later";
-                        break;
-                    case Error::not_modified:
-                        echo "Niet gewijzigd";
-                        break;
-                    default:
-                        if ($executed) {
-                            if ($succes)
-                                if ($last_id)
-                                    echo "Toegevoegd met id: $last_id";
-                                else
-                                    echo "Toegevoegd / aangepast";
-                            else
-                                echo "Er is iets misgelopen";
-                        }
-                }
-                echo "</div>";
-            }
+        public static final function get_error_messages() {
+            return json_decode(file_get_contents(__LANGUAGE_JSON_FILE_NAME_ERROR_MESSAGES__), true)[language()];
         }
     }
